89330eb1b987a18afb09d6688a7756498c5d388a
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-03 20:26:31 -0300

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newvision::Application.config.secret_token = '0e1df531529657d5693621d90f0fa412cc31e5793811a64bb48f87b038519491d24cf0ef9b763aa239bdc78ddefff9bac438c25120d1791e96eef8e3c9a2b3ec'
