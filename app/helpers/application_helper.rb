34fe711e4e217e4046b1dff6814525c65805ed26
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-07 20:16:26 -0300

    Kind of background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def load_menu(role)
+    menu = YAML::load(File.open("config/menus/menu_#{role}.yaml"))
+    h = "<ul>"
+    menu.each do |m|
+      h += "<li> #{m["title"]}</li>"
+    end
+    h += "</ul>"
+    h
+  end
 end
